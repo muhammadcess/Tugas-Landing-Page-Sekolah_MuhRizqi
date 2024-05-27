9d1699e7875750d2b3ee09647aab6ac564aff86f
muhamnmadcess <rizqismuhamad@gmail.com>
2024-05-27 15:11:49 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Profile from './components/Profile';
+import Features from './components/Features';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <Profile />
+      <Features />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Footer() {
+  return (
+    <footer className="bg-dark text-white text-center py-3" id="contact">
+      <div className="container">
+        <p>Contact us at: info@mylandingpage.com</p>
+        <p>&copy; 2024 My Landing Page. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
